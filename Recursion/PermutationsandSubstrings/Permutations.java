6b3f7c2ef3fcd1e173b6403acf57526d346bd79b
Marko McDowell <marko.mcdowell@gmail.com>
2017-01-31 13:28:01 -0700

    Add files via upload

---
+++ Recursion/PermutationsandSubstrings/Permutations.java
@@ -0,0 +1,64 @@
+package Permutations;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * This program computes permutations of a string.
+ */
+public class Permutations
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What word to permutate?");
+		String goats = sc.next();
+		for (String s : permutations(goats))
+		{
+			System.out.println(s);
+		}
+		sc.close();
+	}
+	
+	/**
+	 * Gets all permutations of a given word.
+	 * 
+	 * @param word
+	 *            the string to permute
+	 * @return a list of all permutations
+	 */
+	public static ArrayList<String> permutations(String word)
+	{
+		ArrayList<String> result = new ArrayList<String>();
+		
+		// The empty string has a single permutation: itself
+		if (word.length() == 0)
+		{
+			result.add(word);
+			return result;
+		} else
+		{
+			// Loop through all character positions
+			for (int i = 0; i < word.length(); i++)
+			{
+				// Form a shorter word by removing the ith character
+				String shorter = word.substring(0, i) + word.substring(i + 1);
+				
+				// Generate all permutations of the simpler word
+				ArrayList<String> shorterPermutations = permutations(shorter);
+				
+				// Add the removed character to the front of
+				// each permutation of the simpler word,
+				
+				for (String s : shorterPermutations)
+				{
+					result.add(word.charAt(i) + s);
+				}
+				
+			}
+			// Return all permutations
+			return result;
+		}
+	}
+	
+}
---
+++ Recursion/PermutationsandSubstrings/Substrings.java
@@ -0,0 +1,32 @@
+package Permutations;
+
+import java.util.Scanner;
+
+public class Substrings
+{
+	
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Computer! Tell me the substring of:");
+		String str = sc.next();
+		sbstr(str);
+		sc.close();
+	}
+	
+	public static void sbstr(String word)
+	{
+		if (word.length() == 1)
+		{
+			System.out.println(word);
+			return;
+		} else
+		{
+			System.out.println(word);
+			sbstr(word.substring(0, word.length() - 1));
+			sbstr(word.substring(1, word.length()));
+		}
+		
+	}
+	
+}
