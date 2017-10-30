4b81c3b2a48a8017ecc5991854e862282ca35a9d
Marko McDowell <marko.mcdowell@gmail.com>
2017-10-30 09:21:45 -0600

    Add files via upload

---
+++ PINTernary/PIN.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+//This is P5.9 from the Big Java 5th Edition
+class PIN
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int failedAttempts = 0;
+		boolean Running = true;
+
+		while (Running == true)
+		{
+			if(failedAttempts < 3)
+			{
+				System.out.println("INSERT PIN");
+				int input = sc.nextInt();
+				String out = input == 1234 ? "Your PIN is Correct" : "Your PIN is incorrect";
+				System.out.println(out);
+				if (out.equals("Your PIN is Correct"))
+				{
+					Running = false;
+				}
+			}
+
+			else
+			{
+				System.out.println("YOUR CARD HAS BEEN BLOCKED");
+				Running = false;	
+			}
+		}	
+		
+	}
+}
